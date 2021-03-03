6f24361747b54c70d3cc0073ed4aecc8e10a7272
“Sanjana <sbsanjana@gmail.com>
2021-03-03 17:42:55 -0500

    component library added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from "react";
+import { Button } from 'antd';
+import "antd/dist/antd.css";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <Button type="primary">Primary</Button>
+    </div>
+  )
 }
